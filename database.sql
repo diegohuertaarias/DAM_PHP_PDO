28a6c2131e328e2713b4bcfb25ac7957a9bd9035
usuario1 <ejemplo@gmail.com>
2024-01-24 11:35:27 +0100

    primer envio

---
+++ database.sql
@@ -0,0 +1,12 @@
+CREATE SCHEMA `practica` ;
+use `practica`;
+CREATE TABLE `practica`.`productos` (
+                                        `id` INT NOT NULL AUTO_INCREMENT,
+                                        `nombre_de_producto` VARCHAR(45) NULL,
+                                        `unidades` INT NULL,
+                                        `precio` FLOAT NULL,
+                                        `caducidad` DATE NULL,
+                                        `descuento` TINYINT NULL,
+                                        `foto` VARCHAR(250) NULL,
+                                        PRIMARY KEY (`id`)
+                                    );
